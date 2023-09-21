ea0a19f20755b3e4e9b1483c56727e22681365b7
brooklinlee <141959146+brooklinlee@users.noreply.github.com>
2023-09-20 19:02:23 -0600

    create postSchema

---
+++ models/post.js
@@ -0,0 +1,18 @@
+import mongoose from 'mongoose'
+
+const Schema = mongoose.Schema
+
+const postSchema = new Schema({
+  title: String,
+  content: String,
+  public: Boolean,
+  author: { type: Schema.Types.ObjectId, ref: 'Profile' }
+}, {
+  timestamps: true
+})
+
+const Post = mongoose.model('Post', postSchema)
+
+export {
+  Post
+}
