fe943066e3062a76cf0c6ea39b1541d36305da4f
brooklinlee <141959146+brooklinlee@users.noreply.github.com>
2023-09-21 18:57:36 -0600

    create route to submit new entry form and redirect to the posts/index view

--- controllers/posts.js
+++ controllers/posts.js
@@ -5,10 +5,40 @@ function newPost(req, res) {
   res.render('posts/new', {
     title: 'New Whimsy Entry'
   })
+}
 
+function create(req, res) {
+  req.body.public = !!req.body.public
+  // req.body.owner = req.user.profile._id
+  Post.create(req.body)
+  .then(post => {
+    res.redirect('/posts')
+  })
+  .catch(err => {
+    console.log(err)
+    console.log('❌❌❌')
+    res.redirect('/')
+  })
+}
+
+function index(req, res) {
+  Post.find({})
+  .then(posts => {
+    res.render('posts/index', {
+      posts,
+      title: 'Whimsy Journal'
+    })
+  })
+  .catch(err => {
+    console.log(err)
+    console.log('❌❌❌')
+    res.redirect('/')
+  })
 }
 
 
 export {
   newPost as new,
+  create,
+  index,
 }
--- routes/posts.js
+++ routes/posts.js
@@ -1,9 +1,12 @@
 import { Router } from 'express'
 import * as postsCtrl from '../controllers/posts.js'
+import { isLoggedIn } from '../middleware/middleware.js'
 
 const router = Router()
 
 router.get('/new', postsCtrl.new)
+router.get('/', postsCtrl.index)
+router.post('/', isLoggedIn, postsCtrl.create)
 
 
 export {
