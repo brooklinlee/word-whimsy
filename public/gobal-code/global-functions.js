f8f5c3c4cffce49eb43dfbfb394ae2f9409ad72c
brooklinlee <141959146+brooklinlee@users.noreply.github.com>
2023-09-27 12:29:16 -0600

    Fix the edit comment form so that it renders on the click of the edit button

---
+++ public/gobal-code/global-functions.js
@@ -0,0 +1,8 @@
+const editBtns = document.querySelectorAll('#edit-btn')
+const editForms = document.querySelectorAll('.edit-comment-form')
+
+editBtns.forEach((btn, idx) => {
+  btn.addEventListener('click', () => {
+    editForms[idx].style.display = editForms[idx].style.display === "flex" ? "none" : "flex"
+  })
+})
--- public/stylesheets/global-functions.js
+++
@@ -1,8 +0,0 @@
-const editBtns = document.querySelectorAll('.edit-btn')
-const editForms = document.querySelectorAll('.edit-comment-form')
-
-editBtns.forEach((btn, idx) => {
-  btn.addEventListener('click', () => {
-    editForms[idx].style.display = editForms[idx].style.display === "none" ? "flex" : "none"
-  })
-})
